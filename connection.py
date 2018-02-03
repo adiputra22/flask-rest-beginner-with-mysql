ae7b12458a066a0447e5786592521cd2f5d82217
adiputra22 <tuts.adiputra@gmail.com>
2018-02-03 08:36:09 +0700

    make default mysql at linux

--- connection.py
+++ connection.py
@@ -1,3 +1,9 @@
 import pymysql.cursors
 
-connection = pymysql.connect(host='localhost',user='root',password='root',unix_socket="/Applications/MAMP/tmp/mysql/mysql.sock",db='flask-rest-mysql',port=3306)
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    db='flask-rest-mysql',
+    port=3306
+)
